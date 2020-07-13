0e43439c1788a847be5417e46e4ef8ef4e14c48e
Kevin Abrar Khansa <kevariable@gmail.com>
2020-07-13 11:36:32 +0700

    Preventing login after register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -36,5 +38,38 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            // https://laravel.com/docs/7.x/validation#rule-email
+            $this->username() => [
+                'required', 'string',
+                Rule::exists('users')->where(function ($query) {
+                    $query->where('active', true);
+                })
+            ],
+            'password' => 'required|string'
+        ], $this->validateError());
+    }
+
+    /*
+ * Get validation error for login
+ * @return array
+ * */
+    protected function validateError()
+    {
+        return [
+            $this->username() . '.exists' => 'The selected email is invalid or you need to activate your account'
+        ];
     }
 }
